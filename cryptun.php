7c5dee34a59f19d691db9d8f4740221afdfee366
ErickMarlo <erickmarlo8@gmail.com>
2016-08-05 23:42:44 +0300

    Create cryptun.php

---
+++ cryptun.php
@@ -0,0 +1,22 @@
+<?php
+class cryptun {
+  function en($txt,$key='SoMePaSsword K8s',$alg='twofish',$mod ='ecb')
+  {
+
+      $iv = mcrypt_create_iv(mcrypt_get_iv_size($alg, MCRYPT_MODE_ECB), MCRYPT_RAND);
+      $encrypted_string = mcrypt_encrypt($alg, $key, $txt, MCRYPT_MODE_ECB, $iv);
+      return base64_encode($encrypted_string);
+  }
+
+  function de($txt,$key='SoMePaSsword K8s',$alg='twofish',$mod ='ecb')
+  {
+      $txt = base64_decode($txt);
+
+      $iv = mcrypt_create_iv(mcrypt_get_iv_size($alg, $mod), MCRYPT_RAND);
+      $decrypted_string = mcrypt_decrypt($alg, $key,  $txt, $mod, $iv);
+      return trim($decrypted_string);
+  }
+
+}
+
+?>
